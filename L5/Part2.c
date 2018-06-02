c01fed6f0f29ef9ac783781faffd34c484f5afa5
XLJuice <35495987+XLJuice@users.noreply.github.com>
2018-06-02 13:29:36 +0300

    Add files via upload

---
+++ L5/Part2.c
@@ -0,0 +1,104 @@
+#include <math.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <locale.h>
+#define SIGN_FUNC(f, x) ((f(x) > 0)? 1 : (f(x) < 0)? -1 : 0)
+#define CHECK_ROOT_TOL(f, x, e)\
+if (fabs(f(x)) <= (e))\
+{\
+return x;\
+}
+#define CHECK_DIFF_SIGN(s1, s2) s1 != s2
+#define PRINT_MID_STAGE
+
+typedef double(*funct_t)(double);
+int localize_root(double, double, funct_t);
+double bisection(double, double, double, funct_t);
+double secant(double, double, double, funct_t);
+double Newton(double, double, double, funct_t, funct_t);
+double f(double);
+double df(double);
+
+int main() {
+	setlocale(LC_ALL,"");
+	double eps = 1e-6;
+	double a=-10, b=10, x, h=0.01;
+	for (x = a; x < b; x += h)
+	{
+		if (localize_root(x, x + h, f))
+		{
+			printf("Интервал [%.3f, %.3f]\n", x, x + h);
+			double x0 = bisection(x, x + h, eps, f);
+			printf("Корень: %lf\n", x0);
+			double x1 = secant(a, b, eps, f);
+			printf("Корень: %lf\n", x1);
+			double x2 = Newton(a, b, eps, f, df);
+			printf("Корень: %lf\n", x2);
+		}
+	}
+	getchar();
+	return 0;
+}
+
+int localize_root(double a, double b, funct_t f)
+{
+	return SIGN_FUNC(f, a) != SIGN_FUNC(f, b);
+}
+
+double bisection(double a, double b, double eps, funct_t f) {
+	double funcA, funcB, funcC, c=0;
+	printf("---------------------Метод бисекции----------------------\n");
+	while (fabs(a - b) > eps)
+	{
+		double c = (a + b) / 2;
+		int sgn1 = SIGN_FUNC(f, a);
+		int sgn2 = SIGN_FUNC(f, b);
+		int sgn3 = SIGN_FUNC(f, c);
+#ifdef PRINT_MID_STAGE
+		printf("%.5f\t%.5f\t%.5f\n", a, c, b);
+		printf("%d\t%d\t%d\n", sgn1, sgn3, sgn2);
+
+#endif
+			CHECK_ROOT_TOL(f, a, eps);
+		CHECK_ROOT_TOL(f, c, eps);
+		CHECK_ROOT_TOL(f, b, eps);
+		if (CHECK_DIFF_SIGN(sgn1, sgn3))
+		{
+			b = c;
+		}
+		else {
+			if (CHECK_DIFF_SIGN(sgn3, sgn2))
+			{
+				a = c;
+			}
+		}
+	}
+	return c;
+}
+
+double secant(double a, double b, double eps, funct_t f){
+	printf("-----------------------Метод хорд------------------------\n");
+	do{
+		a = b - ((b - a) * f(b)) / (f(b) - f(a));
+		b = a - ((a - b) * f(a)) / (f(a) - f(b));
+	} while (fabs(f(b)) > eps);
+	return b;
+}
+
+double Newton(double a, double b, double eps, funct_t f, funct_t df) {
+	printf("--------------------Метод косательных--------------------\n");
+	a = b + 2 * eps;
+	do{
+		a = b;
+		b = b - f(b) / df(b);
+	} while (fabs(b - a) > eps);
+	return b;
+}
+
+double f(double x){
+	return cos(x) - 2*x + 1.5;
+}
+
+double df(double x) {
+	return - sin(x)- 2;
+}
---
+++ L5/Part3.c
@@ -0,0 +1,45 @@
+#include <math.h>
+#include <stdio.h>
+#include <locale.h>
+#include <tchar.h>
+typedef double(*quad_func_t)(double);
+double trapez(double, double, double, quad_func_t);
+double f(double);
+double res();
+
+int main()
+{
+    setlocale(LC_ALL,"");
+	double a = -2, b = 2, eps = 1e-5, S;
+	S = trapez(a, b, eps, f);
+	double I = res();
+	printf("Вычисленно интеграл:\nI=%.5f\nСравниваемый результат\nI=%.5f\n", S, I);
+	getchar();
+	return 0;
+}
+
+double trapez(double a, double b, double eps, quad_func_t f)
+{
+	double S = 0, x, h = 0.01;
+	x = a;
+	while (x < b)
+	{
+		S = S + (f(x + h) + f(x))*h / 2;
+		if (0.5*(f(x + h) + f(x))*h >= eps)
+			x = x + h;
+		else
+			break;
+
+	}
+	return S;
+}
+
+double f(double x)
+{
+	return fabs(x+1)/sqrt(x+3);
+}
+
+double res()
+{
+	return (10*sqrt(5)-8*sqrt(2)-10)/3 +8*sqrt(2)-4*sqrt(5);
+}
